c1f8cd5d25755bea143d428194b2413ef8c30374
TheBigSadowski <joe.sadowski@gmail.com>
2013-03-15 23:32:54 -0700

    some nice grouping of data by hour... much better looking charts

--- server.js
+++ server.js
@@ -2,47 +2,51 @@ var http = require('http');
 var util = require('util');
 var azure = require('azure');
 var _ = require('underscore');
+var fs = require('fs');
 
+var storage = JSON.parse(fs.readFileSync('./accounts.json'));
 
-var tableService = azure.createTableService();
+//var tableService = azure.createTableService();
 
 var data = {};
 
-var processResponse = function(err, results, raw) {
-	if (err) console.log(err);
-	_(results).each(function(entity) {
-		var time = data[entity.PartitionKey] || (data[entity.PartitionKey] = {});
-		time[entity.RowKey] = { error: entity.Error, success: entity.Success };
-	});
-	console.log(results.length + ' results added');
-	console.log('next: ' + raw.nextPartitionKey + ' - ' + raw.nextRowKey);
-	if (raw.hasNextPage()) {
-		var nextPageQuery = azure.TableQuery
-			.select()
-			.from('timeData')
-			.whereNextKeys(raw.nextPartitionKey, raw.nextRowKey);
-		azure.createTableService().queryEntities(nextPageQuery, processResponse);
-	} else {
-		results = [];
-		for (var time in data) {
-			var error = 0;
-			var success = 0;
-			for (var instance in data[time]) {
-				error += data[time][instance].error;
-				success += data[time][instance].success;
+_(storage).each(function(account) {
+	var tableService = azure.createTableService(account.account, account.key);
+	var processResponse = function(err, results, raw) {
+		if (err) console.log(err);
+		_(results).each(function(entity) {
+			var time = data[entity.PartitionKey] || (data[entity.PartitionKey] = {});
+			time[account.account + '.' + entity.RowKey] = { error: entity.Error, success: entity.Success };
+		});
+		console.log(results.length + ' results added from ' + account.account);
+		console.log('next: ' + raw.nextPartitionKey + ' - ' + raw.nextRowKey);
+		if (raw.hasNextPage()) {
+			var nextPageQuery = azure.TableQuery
+				.select()
+				.from('timeData')
+				.whereNextKeys(raw.nextPartitionKey, raw.nextRowKey);
+			tableService.queryEntities(nextPageQuery, processResponse);
+		} else {
+			results = [];
+			for (var time in data) {
+				var error = 0;
+				var success = 0;
+				for (var instance in data[time]) {
+					error += data[time][instance].error;
+					success += data[time][instance].success;
+				}
+				results.push([time, error, success]);
 			}
-			results.push([time, error, success]);
-			//console.log(time + '  e:' + error + '  s:' + success);
+			console.log(_.sortBy(results, function(val) { return val[0]; }));
 		}
-		console.log(results);
-	}
-};
+	};
 
-var query = azure.TableQuery
-	.select()
-	.from('timeData');
+	var query = azure.TableQuery
+		.select()
+		.from('timeData');
 
-tableService.queryEntities(query, processResponse);
+	tableService.queryEntities(query, processResponse);
+});
 
 /*
 azure.createTableService().queryTables(function(err, results) {
