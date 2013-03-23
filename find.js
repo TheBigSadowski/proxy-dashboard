5183679744e8d78ea9920b312c36cb28161c6191
TheBigSadowski <joe.sadowski@gmail.com>
2013-03-23 09:07:03 -0700

    adding scripts to find more of the data

---
+++ find.js
@@ -0,0 +1,36 @@
+﻿var azure = require('azure');
+var fs = require('fs');
+
+var ts = azure.createTableService();
+
+var testValue = process.argv[2];
+console.log('Searching for: [' + testValue + '] within the last hour');
+
+
+String.prototype.contains = function (it) { return this.indexOf(it) != -1; };
+
+
+var sixtyMinutesAgo = new Date().getTime() - 1 * 5 * 60 * 1000; //hours * minutes * seconds * miliseconds
+var ticks = ((sixtyMinutesAgo * 10000) + 621355968000000000) // microseconds * windows epoch
+var firstKey = '0' + ticks;
+
+var findData = function (nextPartitionKey, nextRowKey) {
+    var query = azure.TableQuery
+        .select('Message')
+        .from('WADLogsTable')
+        .whereNextKeys(nextPartitionKey || firstKey, nextRowKey || '');
+
+    ts.queryEntities(query, function (err, results, raw) {
+        if (raw.hasNextPage()) {
+            findData(raw.nextPartitionKey, raw.nextRowKey);
+        }
+        for (var i = 0; i < results.length; i++) {
+            if (results[i].Message.contains(testValue)) {
+                fs.appendFile('results.txt', results[i].Message + '\n\n');
+                console.log(results[i].Message);
+            }
+        }
+    });
+};
+
+findData();
---
+++ top-urls.js
@@ -0,0 +1,67 @@
+﻿var azure = require('azure');
+var fs = require('fs');
+var _ = require('underscore');
+
+console.log('Starting at:' + new Date());
+var urls = {};
+
+var port = process.env.PORT || 8888;
+
+var accounts = [];
+for (var i = 0; process.env['AZURE_STORAGE_ACCOUNT_' + i]; i++) {
+	storage.push({
+		name: process.env['AZURE_STORAGE_ACCOUNT_' + i],
+		key: process.env['AZURE_STORAGE_ACCESS_KEY_' + i]
+	});
+}
+
+
+var sixtyMinutesAgo = new Date().getTime() - 1 * 60 * 60 * 1000; //hours * minutes * seonds * miliseconds
+var ticks = ((sixtyMinutesAgo * 10000) + 621355968000000000) // microseconds * windows epoch
+var firstKey = '0' + ticks;
+
+_.each(accounts, function (account) {
+    account.tableService = azure.createTableService(account.name, account.key);
+    account.findData = function (nextPartitionKey, nextRowKey) {
+        var query = azure.TableQuery
+            .select('Message')
+            .from('WADLogsTable')
+            .whereNextKeys(nextPartitionKey || firstKey, nextRowKey || '');
+
+        account.tableService.queryEntities(query, function (err, results, raw) {
+            if (raw.hasNextPage()) {
+                account.findData(raw.nextPartitionKey, raw.nextRowKey);
+            } else {
+                console.log(account.name + ' done');
+            }
+            _.each(results, function (result) {
+                var match = /Original URL: (.*)\n/.exec(result.Message);
+                if (!match) return;
+                var url = match[1];
+                //console.log(url);
+                urls[url] = (urls[url] || 0) + 1;
+            });
+        });
+    };
+    account.findData();
+});
+
+var printResults = function () {
+    _.chain(urls)
+        .map(function (value, key) {
+            return { url: key, count: value };
+        })
+        .sortBy(function (diff) {
+            return diff.count;
+        })
+        .last(10)
+        .reverse()
+        .each(function (diff) {
+            console.log(diff.count + ' - ' + diff.url);
+        });
+    console.log('Done at:' + new Date());
+};
+
+//setInterval(printResults, 10000);
+
+process.on('exit', printResults);
